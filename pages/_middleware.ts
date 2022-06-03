bca67b89cd7f79d32732cb289be3173c2599edd8
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-06-03 16:35:08 +0700

    i

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,12 +1,15 @@
-import { chain, nextSafe, strictDynamic } from "@next-safe/middleware";
+import { chain, nextSafe, strictDynamic, reporting } from "@next-safe/middleware";
 
 const isDev = process.env.NODE_ENV !== "production";
+
+const reportOnly = !!process.env.CSP_REPORT_ONLY;
 
 const middleware = nextSafe((req) => {
     const { origin } = req.nextUrl;
     return {
         isDev,
         contentSecurityPolicy: {
+            reportOnly: reportOnly ?? true,
             "default-src": ["'self' blob:", origin, "https://pokeapi.co"],
             "img-src": ["'self'", origin, "https: data:"],
             "connect-src": ["'self'", origin, "https://pokeapi.co"],
@@ -16,4 +19,21 @@ const middleware = nextSafe((req) => {
     };
 });
 
-export default chain(middleware, strictDynamic());
+const reportingMiddleware = reporting((req) => {
+    const nextApiReportEndpoint = `/api/reporting`;
+    return {
+        csp: {
+            reportUri: process.env.CSP_REPORT_URI || nextApiReportEndpoint,
+        },
+        reportTo: {
+            max_age: 1800,
+            endpoints: [
+                {
+                    url: process.env.REPORT_TO_ENDPOINT_DEFAULT || nextApiReportEndpoint,
+                },
+            ],
+        },
+    };
+});
+
+export default chain(middleware, strictDynamic(), reportingMiddleware);
